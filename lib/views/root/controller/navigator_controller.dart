7a02755b0b27fcc5e8ce4e982420e8f273af27ad
Mohamed Malkia <mmimib001@gmail.com>
2022-12-09 22:37:21 +0100

    root navigation

--- lib/views/root/controller/navigator_controller.dart
+++ lib/views/root/controller/navigator_controller.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class NavigatorController extends GetxController {}
+class NavigatorController extends GetxController {
+  RxInt index = 0.obs;
+
+  Widget get screen => screens[index.value];
+
+  List<Widget> screens = [
+    Container(color: Colors.amber),
+    Container(color: Colors.blue),
+    Container(),
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+  ];
+
+  void goto(int i) {
+    index.value = i;
+    update();
+  }
+}
--- lib/views/root/root.dart
+++ lib/views/root/root.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:mejor_oferta/core/api/authenticator.dart';
-import 'package:mejor_oferta/meta/models/user.dart';
+import 'package:get/get.dart';
+import 'package:mejor_oferta/meta/utils/constants.dart';
+import 'package:mejor_oferta/views/root/controller/navigator_controller.dart';
+import 'package:unicons/unicons.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends GetView<NavigatorController> {
   const RootScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: FutureBuilder<User?>(
-        future: Authenticator.instance.getUser(),
-        builder: (context, snapshot) {
-          if (snapshot.data == null) return Container();
-          final user = snapshot.data!;
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(user.name),
-                Text(user.email),
-                Text(user.phone),
-              ],
+    return Obx(
+      () {
+        return Scaffold(
+          body: controller.screen,
+          floatingActionButton: FloatingActionButton(
+            elevation: 0,
+            onPressed: () {},
+            backgroundColor: kPrimaryColor,
+            child: const Icon(
+              UniconsLine.camera,
+              size: 30,
             ),
-          );
-        },
-      ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: (value) => value == 2 ? null : controller.goto(value),
+            currentIndex: controller.index.value,
+            selectedItemColor: kPrimaryColor,
+            unselectedItemColor: Colors.black,
+            selectedFontSize: 0,
+            unselectedFontSize: 0,
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(UniconsLine.home_alt),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(UniconsLine.comment),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: SizedBox.shrink(),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(UniconsLine.tag_alt),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(UniconsLine.user),
+                label: "",
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
