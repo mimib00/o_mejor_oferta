66263cce0ffcf61d38cace9f62401062f79dde43
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2023-02-22 13:48:59 +0100

    fix translation bug

--- lib/views/boost/boost_how.dart
+++ lib/views/boost/boost_how.dart
@@ -14,21 +14,21 @@ class BoostHowScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
         child: Column(
-          children: const [
+          children: [
             HowTile(
               image: "assets/phone1.png",
-              title: "boost_how_1_title",
-              info: "boost_how_1_msg",
+              title: "boost_how_1_title".tr,
+              info: "boost_how_1_msg".tr,
             ),
             HowTile(
               image: "assets/phone2.png",
-              title: "boost_how_2_title",
-              info: "boost_how_2_msg",
+              title: "boost_how_2_title".tr,
+              info: "boost_how_2_msg".tr,
             ),
             HowTile(
               image: "assets/phone3.png",
-              title: "boost_how_3_title",
-              info: "boost_how_3_msg",
+              title: "boost_how_3_title".tr,
+              info: "boost_how_3_msg".tr,
             ),
           ],
         ),
